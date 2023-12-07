faf1be2f38055870b11be11edec73180f13a0b3b
Jperparas <j.s.perpars@gmail.com>
2023-12-07 15:42:27 -0800

    fixed api

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,9 @@
 import  axios  from "axios";
 export default axios.create({
+    baseURL:'https://api.rawg.io/api',
     params:{
-        baseURL:'https://api.rawg.io/api',
-        key:'39831e258b4a4005862d0e8d10d0de8d'
+        key:'39831e258b4a4005862d0e8d10d0de8d',
 
-    }
+    },
 
-})
+});
